6acf0aa6d57370bb06426ace114492addfc3a9bf
April Howard <ahoward@zipcodes-MBP-5.fios-router.home>
2020-02-23 18:51:43 -0500

    Define instance variables. Defining construction

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,44 @@
 package io.zipcoder;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+    private int numberOfExamsTaken;
+
+    public Student() {
+        this.firstName = " ";
+        this.lastName = " ";
+        this.examScores[] =48.00;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    // public void setExamScores(ArrayList<Double>examScores) {
+    //  this.examScores = examScores;
+    // }
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return numberOfExamsTaken;
+    }
+
 }
